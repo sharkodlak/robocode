e60200832bb8ef48ca4b7b768c4b46748d2e4612
Pavel Stetina <github@shark.kom.cz>
2018-02-23 16:24:51 +0100

    Adapt RobotStatus to use Vector

--- src/misc/RobotStatus.java
+++ src/misc/RobotStatus.java
@@ -1,13 +1,15 @@
 package sharkodlak.robocode.misc;
 
+import sharkodlak.geometry.*;
+
 public class RobotStatus extends Position {
-	private final double energy, heading, velocity;
+	private final Vector headingAndVelocity;
+	private final double energy;
 	private final long time;
 
 	public RobotStatus(double x, double y, double heading, double velocity, double energy, long time) {
 		super(x, y);
-		this.heading = heading;
-		this.velocity = velocity;
+		headingAndVelocity = new Vector(heading, velocity);
 		this.energy = energy;
 		this.time = time;
 	}
@@ -17,11 +19,11 @@ public class RobotStatus extends Position {
 	}
 
 	public double getHeading() {
-		return heading;
+		return headingAndVelocity.getAngle();
 	}
 
 	public double getVelocity() {
-		return velocity;
+		return headingAndVelocity.getMagnitude();
 	}
 
 	public long getTime() {
